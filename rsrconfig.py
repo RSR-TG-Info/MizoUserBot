3d802d31a2771e9640dde36d11c0b2b8a09bee93
RSR <80620448+RSR-TG-Info@users.noreply.github.com>
2023-07-22 12:35:31 +0530

    Create rsrconfig.py

---
+++ rsrconfig.py
@@ -0,0 +1,15 @@
+class Config(object):
+
+    # my.telegram.org atanga lak tur
+    API_ID =  API ID # API ID tih hi i API ID in i thlak ang
+   
+    # my.telegram.org atanga lak tur
+    API_HASH = "API hash" # API hash tih hi i API hash in i thlak ang
+
+    #String Session
+    STRING_SESSION = "string session" # string session tih hi i string session in i thlak ang
+
+    #Sudo Users
+    SUDO = [1060318977] # Bracket chhungah hian command hmang thei tura i duh te user ID i dah ang, pakhat aia tam i dah dawn chuan tiang hian i dah ang [163634646, 532636444, 425252626]
+
+   
