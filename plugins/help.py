4c1fffbe460d5020cca0b26c762aa6564a96bfe5
RSR <80620448+RSR-TG-Info@users.noreply.github.com>
2023-07-22 18:45:57 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from rsrconfig import Config
 
-@Client.on_message(filters.command("help",  prefixes=[".", ","]) & (filters.me | filters.user(Config.SUDO)))
+@Client.on_message(filters.command("help", prefixes=[".", ","]) & (filters.me | filters.user(Config.SUDO)))
 async def mzhelp(client, message):
     await client.send_message(chat_id=message.chat.id, text="Helpline group: @helptereuhte", reply_to_message_id=message.id)
     return
